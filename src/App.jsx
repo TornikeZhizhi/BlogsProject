fb843a9067488ba9dbb11ade289e3e3791ed202e
Ani-Gamtsemlidze <ani.gamcemlidze20@gmail.com>
2023-12-23 00:09:00 +0400

    update categories fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import "./App.css";
+import Home from "./pages/Home";
+import Contact from "./pages/Contact";
+import BlogContext from "./Context/BlogContex";
+
+const router = createBrowserRouter([
+  { path: "/", element: <Home /> },
+  { path: "/contact", element: <Contact /> },
+]);
 
 function App() {
   return (
     <>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <BlogContext>
+        <RouterProvider router={router}></RouterProvider>
+      </BlogContext>
     </>
   );
 }
