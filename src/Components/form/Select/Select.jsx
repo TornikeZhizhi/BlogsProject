2eda0fe51465b5f9789e3ba673ac19ffdcc6290b
Ani-Gamtsemlidze <ani.gamcemlidze20@gmail.com>
2023-12-28 19:11:07 +0400

    update

--- src/Components/form/Select/Select.jsx
+++ src/Components/form/Select/Select.jsx
@@ -8,10 +8,8 @@ import FormControl from "@mui/material/FormControl";
 import Select from "@mui/material/Select";
 import Chip from "@mui/material/Chip";
 import { useUpload } from "../../../context/UploadBlogContext";
-// import CancelIcon from "@material-ui/icons/Cancel";
-import CancelIcon from "@mui/icons-material/Cancel";
-// import _without from "lodash/without";
-import DeleteIcon from "@mui/icons-material/Delete";
+
+import CloseIcon from "@mui/icons-material/Close";
 const ITEM_HEIGHT = 48;
 const ITEM_PADDING_TOP = 8;
 const MenuProps = {
@@ -22,32 +20,38 @@ const MenuProps = {
     },
   },
 };
-
-// function getStyles(name, personName, theme) {
-//   return {
-//     fontWeight:
-//       personName.indexOf(name) === -1
-//         ? theme.typography.fontWeightRegular
-//         : theme.typography.fontWeightMedium,
-//   };
-// }
-
+let catArray = [
+  { id: 1, title: "მარკეტი", background_color: "#FFBB2F" },
+  { id: 2, title: "აპლიკაცია", background_color: "#1CD67D" },
+  { id: 3, title: "ხელოვნური ინტელექტი", background_color: "#B11CD6" },
+  { id: 4, title: "UI/UX", background_color: "#FA5757" },
+  { id: 5, title: "კვლევა", background_color: "#70CF25" },
+  { id: 6, title: "Figma", background_color: "#08D2AE" },
+  { id: 7, title: "დამზადება", background_color: "#08D2AE" },
+  { id: 8, title: "კოპიუტერული მეცნიერება", background_color: "#70CF25" },
+  { id: 9, title: "სამუშაო გარემო", background_color: "#1CD67D" },
+  { id: 10, title: "მუსიკა", background_color: "#08D2AE" },
+  { id: 11, title: "სპორტი", background_color: "#FA5757" },
+  { id: 12, title: "სამშენებლო", background_color: "#B11CD6" },
+  { id: 13, title: "სამედიცინო", background_color: "#FFBB2F" },
+  { id: 14, title: "სხვა", background_color: "#FA5757" },
+];
 export default function MultipleSelectChip({ selectArray, label }) {
   const { handleInputChange } = useUpload();
   const [listName, setListName] = React.useState([]);
-
+  // console.log(listName);
   const handleChange = (e) => {
     handleInputChange(e);
     const {
       target: { value },
     } = e;
+    console.log(value);
     setListName(
       // On autofill we get a stringified value.
       typeof value === "string" ? value.split(",") : value
     );
   };
 
-  console.log(selectArray);
   const handleDelete = (e, value) => {
     e.preventDefault();
     const deleteData = listName.filter((item) => {
@@ -57,10 +61,24 @@ export default function MultipleSelectChip({ selectArray, label }) {
     setListName(deleteData);
   };
 
+  const test = () => {
+    const getBackgroundColors = (names, data) => {
+      return names.map((name) => {
+        const foundItem = data.find((item) => item.title === name);
+        return foundItem ? foundItem.background_color : null;
+      });
+    };
+
+    const names = ["მარკეტი", "სამედიცინო"];
+    const backgroundColors = getBackgroundColors(listName, catArray);
+    console.log(backgroundColors);
+    return backgroundColors;
+  };
+
   return (
     <div>
       <label htmlFor="input">{label} </label>
-      <FormControl sx={{ m: 1, width: 288 }}>
+      <FormControl>
         <InputLabel id="demo-multiple-chip-label">კატეგორია</InputLabel>
         <Select
           className="upload_select"
@@ -73,14 +91,15 @@ export default function MultipleSelectChip({ selectArray, label }) {
           input={<OutlinedInput id="select-multiple-chip" label="Chip" />}
           renderValue={(selected) => (
             <Box sx={{ display: "flex", flexWrap: "wrap", gap: 0.5 }}>
-              {selected.map((value) => (
+              {selected.map((value, index) => (
                 <>
                   <Chip
-                    key={value}
+                    key={index}
                     label={value}
                     clickable
+                    style={{ backgroundColor: test()[index], color: "#fff" }}
                     deleteIcon={
-                      <CancelIcon
+                      <CloseIcon
                         onMouseDown={(event) => event.stopPropagation()}
                       />
                     }
